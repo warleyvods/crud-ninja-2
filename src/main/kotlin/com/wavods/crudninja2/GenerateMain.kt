c69181acdbbce3332f2b0fd7520d8b2879c772f6
Warley <warleyvods@gmail.com>
2024-03-24 16:33:12 -0300

    initial

---
+++ src/main/kotlin/com/wavods/crudninja2/GenerateMain.kt
@@ -0,0 +1,11 @@
+package com.wavods.crudninja2
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class GenerateMain {
+
+    fun actionPerformed(anActionEvent: AnActionEvent) {
+        var project = anActionEvent.project
+
+    }
+}
